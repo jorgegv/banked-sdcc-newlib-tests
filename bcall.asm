84961453f9856445758273b4b51189a0c9655f5b
ZXjogv <zx@jogv.es>
2024-05-19 20:20:27 +0200

    banked: change banking routine to preserve rom mapping

--- bcall.asm
+++ bcall.asm
@@ -9,23 +9,33 @@ banked_call:
     pop     hl			; Return address into HL
 				; Compiler stuffs 4 bytes there: bank (16 bits) and function addr (16 bits)
 
-    ld      a,(SV_BANKM)	; save current bank register value on the stack
+    ld      a,(SV_BANKM)	; save previous bank register value on the stack
     push    af
 
-    ld      e,(hl)		; Fetch the call address into DE
+    ld      e,(hl)		; Fetch the call address into DE (2 bytes)
     inc     hl
     ld      d,(hl)
     inc     hl
 
-    ld      a,(hl)		; Get the bank number in A
+    ld      a,(hl)		; Get the bank number in A (1 byte)
     inc     hl	
-
     inc     hl			; Skip the 4th byte
 
-    push    hl			; Push the real return address
+    ex      (sp),hl		; HL now contains the real return address (4
+				; bytes past the call instruction); push it
+				; and get the previous bank register value
+				; in H
+
+    push    hl			; push previous bank register value again
+
+    ld      l,a			; save new bank number
+    ld	    a,h			; h = previous bank register value
+    and     0xf8		; mask out bank number
+    or      l			; set new bank number
+				; now A contains the new bank number and the
+				; saved ROM config
 
     ld      bc,ZX_BANK_IOPORT	; prepare register value to switch bank
-    or      0x10		; A contains the bank nr
     ld      (SV_BANKM),a	; save the new bank value
     out     (c),a		; ...and do the switch
 
@@ -37,9 +47,7 @@ banked_call:
 
     di				; again, critical section start
 
-    pop     bc			; Get the return address into BC
     pop     af			; Pop the old bank
-    push    bc			; Push again return address
 
     ld      bc,ZX_BANK_IOPORT	; restore switch to the old bank and save
     ld      (SV_BANKM),a	; the bank register value
