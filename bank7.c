d5db0262ba124d6737780c6f32082b9a9fcdabd5
ZXjogv <zx@jogv.es>
2024-05-19 19:41:21 +0200

    banked: parameter passing test, with error (WIP)

--- bank7.c
+++ bank7.c
@@ -1,5 +1,8 @@
+#include <stdio.h>
+
 #pragma bank 7
 
-int banked_function7( void ) __banked {
-    return 0x1977;
+int banked_function7( int a ) __banked {
+    printf( "** BANK7.a = 0x%04X\n", a );
+    return 0x0002 * a;
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@ extern int banked_function1( void ) __banked;
 extern int banked_function3( void ) __banked;
 extern int banked_function4( void ) __banked;
 extern int banked_function6( void ) __banked;
-extern int banked_function7( void ) __banked;
+extern int banked_function7( int a ) __banked;
 
 void main( void ) {
     printf( "Hello world from main bank!\n" );
@@ -14,6 +14,6 @@ void main( void ) {
     printf( "** Value from bank 3: 0x%04x\n", banked_function3() );
     printf( "** Value from bank 4: 0x%04x\n", banked_function4() );
     printf( "** Value from bank 6: 0x%04x\n", banked_function6() );
-    printf( "** Value from bank 7: 0x%04x\n", banked_function7() );
+    printf( "** Value from bank 7: 0x%04x\n", banked_function7(2) );
 //    while ( 1 ) ;
 }
